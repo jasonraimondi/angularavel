080e2e0f05327272c1fc1e569ee194bad8ad62ab
Jason Raimondi <jason@raimondi.us>
2015-08-14 21:56:56 -0400

    config tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
-    protected $baseUrl = 'http://localhost';
+    protected $baseUrl = 'http://angularavel.app';
 
     /**
      * Creates the application.
