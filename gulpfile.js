204eaa48cf8eea74a1f08380212eed478662d263
Jason Raimondi <jason@raimondi.us>
2015-08-14 21:56:26 -0400

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var paths = {
-    'BOWER': './vendor/bower_components/',
-    'DESTINATION': './public/assets/',
-    'SOURCE': './resources/assets/',
-    'NODE': './node_modules/',
-    'VIEWS': './resources/views/',
+	'BOWER': './vendor/bower_components/',
+	'DESTINATION': './public/assets/',
+	'SOURCE': './resources/assets/',
+	'NODE': './node_modules/',
+	'VIEWS': './resources/views/',
 }
 
 var elixir = require('laravel-elixir');
@@ -21,26 +21,26 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
 
-    mix.sass('style.scss', paths.DESTINATION + 'css/style.css', {
-        includePaths : [
-            paths.BOWER + 'foundation/scss',
-            paths.BOWER + 'select2/src/scss'
-        ]}
-    );
+	mix.sass('style.scss', paths.DESTINATION + 'css/style.css', {
+		includePaths: [
+			paths.BOWER + 'foundation/scss',
+			paths.BOWER + 'select2/src/scss'
+		]
+	});
 
-    // mix.scripts([
-    //     paths.NODE + 'angular/angular.js',
-    //     paths.NODE + 'angular-route/angular-route.js',
-    //     paths.NODE + 'angular-resource/angular-resource.js',
-    // ], paths.DESTINATION + 'js/vendor.js', './');
+	// mix.scripts([
+	//     paths.NODE + 'angular/angular.js',
+	//     paths.NODE + 'angular-route/angular-route.js',
+	//     paths.NODE + 'angular-resource/angular-resource.js',
+	// ], paths.DESTINATION + 'js/vendor.js', './');
 
-    // mix.scripts([
-    //     paths.SOURCE + 'js/config/routes.js',
-    //     paths.SOURCE + 'js/services/api.js',
-    //     paths.SOURCE + 'js/controllers/myFirstController.js',
-    //     paths.SOURCE + 'js/controllers/mySecondController.js',
-    // ], paths.DESTINATION + 'js/app.js', './');
+	// mix.scripts([
+	//     paths.SOURCE + 'js/config/routes.js',
+	//     paths.SOURCE + 'js/services/api.js',
+	//     paths.SOURCE + 'js/controllers/myFirstController.js',
+	//     paths.SOURCE + 'js/controllers/mySecondController.js',
+	// ], paths.DESTINATION + 'js/app.js', './');
 
-    mix.browserify(paths.SOURCE + 'js/app.js', paths.DESTINATION + 'js/package.js', './');
+	mix.browserify(paths.SOURCE + 'js/app.js', paths.DESTINATION + 'js/package.js', './');
 
 });
